4366e4f091f61e1da785abd3c462ae17637b5fd7
intlabs <petebirley@gmail.com>
2014-06-04 02:45:38 +0100

    ...

--- getDropboxAccessToken.py
+++ getDropboxAccessToken.py
@@ -13,7 +13,7 @@
 # WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
 # ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-import sys, json, urllib, urllib2, httplib, dropbox, argparse
+import os, sys, json, urllib, urllib2, httplib, dropbox, argparse
 
 
 ########################
